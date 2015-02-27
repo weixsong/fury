beac524c756b8c0f442df113132e94ecf43823fd
weixsong <watkinsong@163.com>
2015-02-27 11:13:45 +0800

    final refactorred

--- AlgorithmPU/src/Solver.java
+++ AlgorithmPU/src/Solver.java
@@ -1,26 +1,23 @@
-
 public class Solver {
-	
-	private class Node implements Comparable<Node>{
+
+	private class Node implements Comparable<Node> {
 		public Board board = null;
-		public Node previous = null;
+		public Node pre = null;
 		public int manhattan = 0;
 		public int moves = 0;
-		
-		public Node(Board b, Node pre) {
+
+		public Node(Board b, Node parent) {
 			board = b;
 			manhattan = b.manhattan();
-			moves = pre == null ? 0 : pre.moves + 1;
-			previous = pre;
+			moves = parent == null ? 0 : parent.moves + 1;
+			pre = parent;
 		}
-		
-		
+
 		@Override
-		public int compareTo(Node o) {
-			Node that = o;
+		public int compareTo(Node that) {
 			if ((this.manhattan + this.moves) > (that.manhattan + that.moves)) {
 				return 1;
-			} else if ((this.manhattan + this.moves) < (that.manhattan + that.moves)){
+			} else if ((this.manhattan + this.moves) < (that.manhattan + that.moves)) {
 				return -1;
 			} else {
 				if (this.manhattan > that.manhattan) {
@@ -33,17 +30,17 @@ public class Solver {
 			}
 		}
 	}
-	
+
 	private int insertNum = 0;
-	
+
 	private MinPQ<Node> pq = null;
 	private MinPQ<Node> pq2 = null;
-	
+
 	private Node current = null;
 	private Node current2 = null;
-	
+
 	private boolean isSolvable;
-    
+
 	// find a solution to the initial board (using the A* algorithm)
 	public Solver(Board initial) {
 		pq = new MinPQ<Node>();
@@ -52,16 +49,19 @@ public class Solver {
 		isSolvable = true;
 
 		Node node = new Node(initial, null);
-		pq.insert(node);insertNum++;
+		pq.insert(node);
+		insertNum++;
 
 		Board twin = initial.twin();
 		Node node2 = new Node(twin, null);
-		pq2.insert(node2);insertNum++;
+		pq2.insert(node2);
+		insertNum++;
 
 		while (!pq.isEmpty() && !pq2.isEmpty()) {
 			current = pq.delMin();
 			current2 = pq2.delMin();
-			
+
+			// test if solvable
 			if (current.board.isGoal()) {
 				break;
 			} else if (current2.board.isGoal()) {
@@ -69,74 +69,79 @@ public class Solver {
 				break;
 			}
 
+			// add neighbors to PQ
 			for (Board b : current.board.neighbors()) {
 				Node n = new Node(b, current);
-				if (current.previous == null) {
-					pq.insert(n);insertNum++;
-				} else if (!current.previous.board.equals(b)) {
-					pq.insert(n);insertNum++;
+				if (current.pre == null) {
+					pq.insert(n);
+					insertNum++;
+				} else if (!current.pre.board.equals(b)) {
+					pq.insert(n);
+					insertNum++;
 				}
 			}
-			
+
 			for (Board b : current2.board.neighbors()) {
 				Node n = new Node(b, current2);
-				if (current2.previous == null) {
-					pq2.insert(n);insertNum++;
-				} else if (!current2.previous.board.equals(b)) {
-					pq2.insert(n);insertNum++;
+				if (current2.pre == null) {
+					pq2.insert(n);
+					insertNum++;
+				} else if (!current2.pre.board.equals(b)) {
+					pq2.insert(n);
+					insertNum++;
 				}
 			}
 		}
 	}
-	
-    // is the initial board solvable?
+
+	// is the initial board solvable?
 	public boolean isSolvable() {
 		return isSolvable;
 	}
-	
-    // min number of moves to solve initial board; -1 if unsolvable
+
+	// min number of moves to solve initial board; -1 if unsolvable
 	public int moves() {
 		if (!isSolvable()) {
 			return -1;
 		}
 		return current.moves;
 	}
-	
-    // sequence of boards in a shortest solution; null if unsolvable
+
+	// sequence of boards in a shortest solution; null if unsolvable
 	public Iterable<Board> solution() {
 		if (!isSolvable()) {
 			return null;
 		}
 		Stack<Board> solution = new Stack<Board>();
-		for (Node t = current; t != null; t = t.previous) {
-			solution.push(t.board);
+		for (Node node = current; node != null; node = node.pre) {
+			solution.push(node.board);
 		}
 		return solution;
 	}
-	
+
 	// solve a slider puzzle (given below)
 	public static void main(String[] args) {
-		 // create initial board from file
+		// create initial board from file
 		System.out.println(args[0]);
-	    In in = new In(args[0]);
-	    int N = in.readInt();
-	    int[][] blocks = new int[N][N];
-	    for (int i = 0; i < N; i++)
-	        for (int j = 0; j < N; j++)
-	            blocks[i][j] = in.readInt();
-	    Board initial = new Board(blocks);
+		In in = new In(args[0]);
+		int N = in.readInt();
+		int[][] blocks = new int[N][N];
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
+				blocks[i][j] = in.readInt();
+		Board initial = new Board(blocks);
 
-	    // solve the puzzle
-	    Solver solver = new Solver(initial);
+		// solve the puzzle
+		Solver solver = new Solver(initial);
 
-	    // print solution to standard output
-	    if (!solver.isSolvable())
-	        StdOut.println("No solution possible");
-	    else {
-	        StdOut.println("Minimum number of moves = " + solver.moves());
-	        for (Board board : solver.solution())
-	            StdOut.println(board);
-	    }
-	    System.out.println(solver.insertNum);
+		// print solution to standard output
+		if (!solver.isSolvable())
+			StdOut.println("No solution possible");
+		else {
+			StdOut.println("Minimum number of moves = " + solver.moves());
+			for (Board board : solver.solution())
+				StdOut.println(board);
+		}
+		System.out.println(solver.insertNum);
 	}
 }
